8ac36117d974e80022c683a6663afb4cf00216c5
Selimzhan <selimzhanozer@gmail.com>
2022-07-01 17:25:29 +0600

    xx

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import asyncio
+import time
+from typing import List
+
 from fastapi import FastAPI, Depends, WebSocket
+from starlette.websockets import WebSocketDisconnect
+
 from alembic.devsettings import redis
 from geopy.geocoders import Nominatim
 from argon2 import PasswordHasher
@@ -11,6 +17,25 @@ app = FastAPI()
 ph = PasswordHasher()
 auth_handler = AuthHandler()
 
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
 html = """
 <!DOCTYPE html>
 <html>
@@ -18,7 +43,7 @@ html = """
         <title>Chat</title>
     </head>
     <body>
-        <h1>WebSocket Chat</h1>
+        <h1>SendData</h1>
         <form action="" onsubmit="sendMessage(event)">
             <input type="text" id="messageText" autocomplete="off"/>
             <button>Send</button>
@@ -27,13 +52,13 @@ html = """
         <ul id='messages'>
         </ul>
         <script>
-            var ws = new WebSocket("ws://localhost:9000/ws");
+            var ws = new WebSocket("ws://localhost:9000/ws/1");
             const getRandom = () => {
               let random = Math.random() * 150;
               let random2 = Math.random() * 150;
               let dictionary = {
-                long: random,
-                latt: random2
+                longitude: random,
+                latitude: random2
               }
               console.log(dictionary)
               ws.send(JSON.stringify(dictionary))
@@ -51,12 +76,11 @@ html = """
                 message.appendChild(content)
                 messages.appendChild(message)
             };
-            
         </script>
     </body>
 </html>
 """
-
+manager = ConnectionManager()
 
 @app.get("/")
 async def get():
@@ -77,17 +101,22 @@ async def login(
         return {"message": "Enter correct password or username"}
 
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_text()
-        print(data)
-        await redis.set('data', data)
-        await websocket.send_text(f"Message text was: {data}")
+# @app.websocket("/ws")
+# async def websocket_endpoint(websocket: WebSocket):
+#     await websocket.accept()
+#     while True:
+#         data = await websocket.receive_text()
+#         await redis.set('data', data)
+#         await websocket.send_text(f"Message text was: {data}")
 
-
-@app.get('/get_data')
-async def data():
-    geo = await redis.get('data')
-    return geo
+@app.websocket("/ws/{user}")
+async def websocket_endpoint(websocket: WebSocket, user: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            await redis.set('data', data)
+            await manager.broadcast(f"{data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast(f"Client #{user} left the chat")
